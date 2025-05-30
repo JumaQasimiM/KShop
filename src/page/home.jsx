680d7e47494175979b12d2c8194a9298a7eaac22
Juma Qasimi <Jumaqasimi@gmail.com>
2025-05-30 10:35:35 +0200

    create reat project and install Tailwindcss using vite

---
+++ src/page/home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="border">
+      <h1 className="text-red-300 ">Home page</h1>
+      <h1 className="bg-red-500">Start 30.05.25</h1>
+    </div>
+  );
+};
+
+export default Home;
